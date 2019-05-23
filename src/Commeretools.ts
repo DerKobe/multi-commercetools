1e2625cb08dde9799425022c31eabb544164773a
Philip Claren <philip.claren@googlemail.com>
2019-05-23 09:51:50 +0200

    inserted functionallity with minor changes

---
+++ src/Commeretools.ts
@@ -0,0 +1,322 @@
+import { createRequestBuilder } from '@commercetools/api-request-builder';
+import { createClient } from '@commercetools/sdk-client';
+import { createAuthMiddlewareForClientCredentialsFlow } from '@commercetools/sdk-middleware-auth';
+import { createHttpMiddleware } from '@commercetools/sdk-middleware-http';
+import { createQueueMiddleware } from '@commercetools/sdk-middleware-queue';
+import fetch from 'node-fetch';
+import {
+  AddAttributeAction, Channel, CustomObject, CustomObjectDraft, Entity, InventoryEntry,
+  InventoryEntryDraft, PagedQueryResult, Product, ProductDraft, ProductType
+} from './types';
+
+interface ICommercetoolsConfig {
+  projectKey: string,
+  clientId: string,
+  clientSecret: string,
+  locale: string,
+  concurrency: number,
+  authHost: string,
+  apiHost: string,
+}
+
+export class Commercetools {
+  public locale: string;
+
+  private readonly config: ICommercetoolsConfig;
+  private client: any;
+  private request: any;
+  private headers: any;
+
+  constructor(config: ICommercetoolsConfig) {
+    this.config = config;
+    this.locale = config.locale;
+  }
+
+  public async initClient(): Promise<any> {
+    if (this.client) {
+      return;
+    }
+
+    const { projectKey, clientId, clientSecret, concurrency, locale, apiHost, authHost } = this.config;
+
+    const httpMiddleware = createHttpMiddleware({ host: apiHost, fetch });
+    const queueMiddleware = createQueueMiddleware({ concurrency });
+    const authMiddleware = createAuthMiddlewareForClientCredentialsFlow({
+      host: authHost,
+      projectKey,
+      fetch,
+      credentials: { clientId, clientSecret },
+    });
+
+    this.headers = { Accept: 'application/json', 'Content-Type': 'application/json' };
+    this.request = () => createRequestBuilder({ projectKey });
+    this.client = createClient({ middlewares: [authMiddleware, httpMiddleware, queueMiddleware] });
+
+    this.locale = locale;
+
+    return this.client;
+  }
+
+  public async resolveKeyAndVersion(keyOrEntity: string | Entity, fetchByKey: Function): Promise<Entity> {
+    switch (typeof keyOrEntity) {
+      case 'object':
+        return new Promise(resolve => {
+          resolve({
+            key: (keyOrEntity as Entity).key,
+            version: (keyOrEntity as Entity).version,
+          });
+        });
+
+      case 'string':
+        return fetchByKey(keyOrEntity)
+          .then((entity: Entity) => ({
+            key: keyOrEntity,
+            version: entity.version,
+          }));
+
+      default:
+        throw new Error(`Invalid value for keyOrEntity: ${JSON.stringify(keyOrEntity)}`);
+    }
+  }
+
+  // --- Orders --- //
+
+  public async fetchExpandedOrder(id: string): Promise<any> {
+    await this.initClient();
+
+    const uri = (
+      this.request()
+        .orders
+        .byId(id)
+        .expand('lineItems[*].productType')
+        .expand('lineItems[*].supplyChannel')
+        .expand('lineItems[*].distributionChannel')
+        .expand('lineItems[*].variant.attributes[*].value')
+        .expand('custom.fields.permissions')
+        .build()
+    );
+
+    const fetchRequest = {
+      uri,
+      method: 'GET',
+      headers: this.headers,
+    };
+
+    return this.client.execute(fetchRequest).then(({ body: order }) => order);
+  }
+
+  // --- Channels --- //
+
+  public async fetchChannelByKey(key: string): Promise<Channel> {
+    await this.initClient();
+
+    const fetchRequest = {
+      uri: this.request().channels.where(`key="${key}"`).build(),
+      method: 'GET',
+      headers: this.headers,
+    };
+
+    return (
+      this.client
+        .execute(fetchRequest)
+        .then(({ body: { results } }) => results[0])
+        .catch(error => {
+          console.dir(error, { depth: null });
+          throw error;
+        })
+    );
+  }
+
+  // --- CustomObjects --- //
+
+  public async fetchCustomObject(container: string, key: string): Promise<CustomObject> {
+    await this.initClient();
+
+    const fetchRequest = {
+      uri: this.request().customObjects.where(`container="${container}" and key="${key}"`).build(),
+      method: 'GET',
+      headers: this.headers,
+    };
+
+    return (
+      this.client
+        .execute(fetchRequest)
+        .then(response => (response.body as PagedQueryResult).results[0])
+    );
+  }
+
+  public async saveCustomObject(customObjectDraft: CustomObjectDraft): Promise<CustomObject> {
+    await this.initClient();
+
+    const postRequest = {
+      uri: this.request().customObjects.build(),
+      method: 'POST',
+      headers: this.headers,
+      body: customObjectDraft,
+    };
+
+    return (
+      this.client
+        .execute(postRequest)
+        .then(response => response.body)
+    );
+  }
+
+  // --- InventoryEntries --- //
+
+  public async fetchInventoryEntry(sku: string, supplyChannelKey: string): Promise<InventoryEntry> {
+    await this.initClient();
+
+    const channelRequest = {
+      uri: this.request().channels.where(`key="${supplyChannelKey}"`).build(),
+      method: 'GET',
+      headers: this.headers,
+    };
+
+    const channel = await this.client.execute(channelRequest).then(({ body: { results } }) => results[0]);
+
+    const inventoryRequest = {
+      uri: this.request().inventory.where(`sku="${sku}" and supplyChannel(typeId="channel" and id="${channel.id}")`).build(),
+      method: 'GET',
+      headers: this.headers,
+    };
+
+    return (
+      this.client
+        .execute(inventoryRequest)
+        .then(({ body: { results } }) => results[0])
+    );
+  }
+
+  public async createInventoryEntry(inventoryEntryDraft: InventoryEntryDraft): Promise<InventoryEntry> {
+    await this.initClient();
+
+    const postRequest = {
+      uri: this.request().inventory.build(),
+      method: 'POST',
+      headers: this.headers,
+      body: inventoryEntryDraft,
+    };
+
+    return (
+      this.client
+        .execute(postRequest)
+        .then(response => response.body)
+    );
+  }
+
+  public async deleteInventoryEntry(id: string, version: number): Promise<void> {
+    await this.initClient();
+
+    const deleteRequest = {
+      uri: this.request().inventory.byId(id).withVersion(version).build(),
+      method: 'DELETE',
+      headers: this.headers,
+    };
+
+    return (
+      this.client
+        .execute(deleteRequest)
+        .catch(error => {
+          console.dir(error, { depth: null });
+          throw error;
+        })
+    );
+  }
+
+  // --- Products --- //
+
+  public async fetchProductByEan(ean: string): Promise<Product> {
+    await this.initClient();
+
+    const fetchRequest = {
+      uri: this.request().products.where(`masterData(current(masterVariant(attributes(name="ean" and value="${ean}"))))`).build(),
+      method: 'GET',
+      headers: this.headers,
+    };
+
+    return (
+      this.client
+        .execute(fetchRequest)
+        .then(response => (response.body as PagedQueryResult).results[0])
+    );
+  }
+
+  public async fetchProductByProductCode(productCode: string): Promise<Product> {
+    await this.initClient();
+
+    const fetchRequest = {
+      uri: this.request().products.where(`masterData(current(masterVariant(attributes(name="productCode" and value="${productCode}"))))`).build(),
+      method: 'GET',
+      headers: this.headers,
+    };
+
+    return (
+      this.client
+        .execute(fetchRequest)
+        .then(response => (response.body as PagedQueryResult).results[0])
+    );
+  }
+
+  public async fetchProductByVariantSku(sku: string): Promise<Product> {
+    await this.initClient();
+
+    const fetchRequest = {
+      uri: this.request().products.where(`masterData(current(masterVariant(sku="${sku}")))`).build(),
+      method: 'GET',
+      headers: this.headers,
+    };
+
+    return (
+      this.client
+        .execute(fetchRequest)
+        .then(response => (response.body as PagedQueryResult).results[0])
+    );
+  }
+
+  public async createProduct(productDraft: ProductDraft): Promise<Product> {
+    await this.initClient();
+
+    const postRequest = {
+      uri: this.request().products.build(),
+      method: 'POST',
+      headers: this.headers,
+      body: productDraft,
+    };
+
+    return (
+      this.client
+        .execute(postRequest)
+        .then(response => response.body)
+    );
+  }
+
+  // --- ProductTypes --- //
+
+  public async fetchProductTypeByKey(key: String): Promise<ProductType> {
+    await this.initClient();
+
+    const fetchRequest = {
+      uri: this.request().productTypes.byKey(key).build(),
+      method: 'GET',
+      headers: this.headers,
+    };
+
+    return this.client.execute(fetchRequest).then(response => response.body);
+  }
+
+  public async updateProductType(keyOrProductType, actions: AddAttributeAction[]): Promise<void> {
+    await this.initClient();
+
+    const { key, version } = await this.resolveKeyAndVersion(keyOrProductType, this.fetchProductTypeByKey);
+
+    const updateRequest = {
+      uri: this.request().productTypes.byKey(key).withVersion(version).build(),
+      method: 'POST',
+      headers: this.headers,
+      body: { version, actions },
+    };
+
+    return this.client.execute(updateRequest);
+  }
+}
---
+++ src/types.ts
@@ -0,0 +1,388 @@
+export interface Entity {
+  key: string;
+  version: number;
+}
+
+export interface Product {
+  id: string;
+  key?: string;
+  version: number;
+  createdAt: DateTime;
+  lastModifiedAt: DateTime;
+  productType: Reference;
+  masterData: ProductCatalogData;
+  taxCategory?: Reference;
+  state?: Reference;
+}
+
+export interface ProductDraft {
+  key?: string;
+  name: LocalizedString;
+  productType: ResourceIdentifier;
+  slug: LocalizedString;
+  description?: LocalizedString;
+  categories?: Array<ResourceIdentifier>;
+  categoryOrderHints?: CategoryOrderHints;
+  metaTitle?: LocalizedString;
+  metaDescription?: LocalizedString;
+  metaKeywords?: LocalizedString;
+  masterVariant?: ProductVariantDraft;
+  variants?: Array<ProductVariantDraft>;
+  taxCategory?: ResourceIdentifier;
+  searchKeywords?: SearchKeywords;
+  state?: Reference;
+  publish?: boolean;
+}
+
+interface ProductVariantDraft {
+  sku?: string;
+  key?: string;
+  prices?: Array<PriceDraft>;
+  images?: Array<Image>;
+  assets?: Array<AssetDraft>;
+  attributes?: Array<Attribute>;
+}
+
+interface Asset {
+  id: string;
+  key?: string;
+  sources: Array<AssetSource>;
+  name: LocalizedString;
+  description?: LocalizedString;
+  tags?: Array<string>;
+  custom?: CustomFields;
+}
+
+interface AssetDraft {
+  key?: string;
+  sources: Array<AssetSource>;
+  name: LocalizedString;
+  description?: LocalizedString;
+  tags?: Array<string>;
+  custom?: CustomFieldsDraft;
+}
+
+interface AssetSource {
+  uri: string;
+  key?: string;
+  dimensions?: AssetDimensions;
+  contentType?: string;
+}
+
+interface CustomFieldsDraft {
+  type: ResourceIdentifier;
+  fields?: any;
+}
+
+interface PriceDraft {
+  value: BaseMoney;
+  country?: string;
+  customerGroup?: Reference;
+  channel?: ResourceIdentifier;
+  validFrom?: DateTime;
+  validUntil?: DateTime;
+  tiers?: Array<PriceTier>;
+  custom?: CustomFieldsDraft;
+}
+
+interface PriceTier {
+  minimumQuantity: number;
+  value: BaseMoney;
+}
+
+interface SearchKeywords {
+  text: string;
+  suggestTokenizer?: SuggestTokenizer;
+}
+
+type SuggestTokenizer = WhitespaceTokenizer | CustomTokenizer;
+
+interface WhitespaceTokenizer {
+  type: 'whitespace';
+}
+
+interface CustomTokenizer {
+  type: 'custom';
+  inputs: Array<string>;
+}
+
+type CategoryOrderHints = any;
+
+type ResourceIdentifier = ResourceIdentifierById | ResourceIdentifierByKey;
+
+interface ResourceIdentifierById {
+  id: string;
+  typeId?: string;
+}
+
+interface ResourceIdentifierByKey {
+  key: string;
+  typeId?: string;
+}
+
+interface Reference {
+  typeId: string;
+  id: string;
+}
+
+interface ProductCatalogData {
+  published: Boolean;
+  current: ProductData;
+  staged: ProductData;
+  hasStagedChanges: Boolean;
+}
+
+interface ProductData {
+  name: LocalizedString;
+  categories: Array<Reference>;
+  description?: LocalizedString;
+  slug: LocalizedString;
+  masterVariant: ProductVariant;
+  variants: Array<ProductVariant>;
+}
+
+interface ProductVariant {
+  id: number;
+  sku?: string;
+  key?: string;
+  prices?: Array<Price>;
+  attributes?: Array<Attribute>;
+  price?: Price;
+  images?: Array<Image>;
+  assets?: Array<Asset>;
+  availability?: ProductVariantAvailability;
+  isMatchingVariant?: Boolean;
+  scopedPrice?: ScopedPrice;
+  scopedPriceDiscounted?: Boolean;
+}
+
+type DateTime = string;
+
+interface Price {
+  id: string;
+  value: BaseMoney;
+  country?: string;
+  customerGroup?: Reference;
+  channel?: Reference;
+  validFrom?: DateTime;
+  validUntil?: DateTime;
+  discounted?: DiscountedPrice;
+  custom?: CustomFields;
+}
+
+interface BaseMoney {
+  type: string;
+  currencyCode: string;
+  centAmount: number;
+  fractionDigits: number;
+}
+
+interface DiscountedPrice {
+  value: Money;
+  discount: Reference;
+}
+
+interface Money {
+  type: string;
+  currencyCode: string;
+  centAmount: number;
+  fractionDigits: number;
+}
+
+interface CustomFields {
+  type: Reference;
+  fields: any;
+}
+
+interface Attribute {
+  name: string;
+  value: any;
+}
+
+interface Image {
+  url: string;
+  dimensions: AssetDimensions;
+  label?: string;
+}
+
+interface AssetDimensions {
+  w: number;
+  h: number;
+}
+
+interface ProductVariantAvailability {
+  isOnStock?: Boolean;
+  restockableInDays?: number;
+  availableQuantity?: number;
+  channels?: any; // Map of ProductVariantAvailability per Channel id
+}
+
+interface ScopedPrice {
+  id: string;
+  value: BaseMoney;
+  currentValue: BaseMoney;
+  country?: string;
+  customerGroup?: Reference;
+  channel?: Reference;
+  validFrom?: DateTime;
+  validUntil?: DateTime;
+  discounted?: DiscountedPrice;
+  custom?: CustomFields;
+}
+
+interface LocalizedString {
+  [locale: string]: string;
+}
+
+export interface ProductType {
+  id: string;
+  key?: string;
+}
+
+export interface AddAttributeAction {
+  action: string;
+  attribute: AttributeDefinitionDraft;
+}
+
+interface AttributeDefinitionDraft {
+  type: AttributeType;
+  name: string;
+  label: LocalizedString;
+  isRequired: boolean;
+  isSearchable?: boolean;
+}
+
+interface AttributeType {
+  name: string;
+}
+
+export interface PagedQueryResult {
+  offset: number;
+  limit: number;
+  count: number;
+  total?: number;
+  results: Array<any>;
+  // facets?: FacetResults;
+  meta?: any;
+}
+
+export interface TaxCategory {
+  id: string;
+  key?: string;
+  version: number;
+  createdAt: DateTime;
+  lastModifiedAt: DateTime;
+  name: string;
+  description?: string;
+  rates: Array<TaxRate>;
+}
+
+interface TaxRate {
+  id?: string;
+  name: string;
+  amount: number; // Percentage in the range of [0..1]
+  includedInPrice: boolean;
+  country: string; // A two-digit country code as per ↗ ISO 3166-1 alpha-2
+  state?: string; // The state in the country
+}
+
+export interface CustomObjectDraft {
+  container: string; // matching the pattern [-_~.a-zA-Z0-9]+
+  key: string; // matching the pattern [-_~.a-zA-Z0-9]+
+  value: any; // JSON types Number, string, Boolean, Array, Object
+  version?: number;
+}
+
+export interface CustomObject {
+  id: string;
+  createdAt: DateTime;
+  lastModifiedAt: DateTime;
+  container: string; // matching the pattern [-_~.a-zA-Z0-9]+
+  key: string; // matching the pattern [-_~.a-zA-Z0-9]+
+  value: any; // JSON types Number, string, Boolean, Array, Object
+  version: number;
+}
+
+export interface InventoryEntry {
+  id: string;
+  version: number;
+  createdAt: DateTime;
+  lastModifiedAt: DateTime;
+  sku: string;
+  supplyChannel?: Reference;
+  quantityOnStock: number;
+  availableQuantity: number;
+  restockableInDays?: number;
+  expectedDelivery?: DateTime;
+  custom?: CustomFields;
+}
+
+export interface InventoryEntryDraft {
+  sku: string;
+  quantityOnStock: number;
+  restockableInDays?: number;
+  expectedDelivery?: DateTime;
+  supplyChannel?: ResourceIdentifier;
+  custom?: CustomFieldsDraft;
+}
+
+export interface Channel {
+  id: string;
+  version: number;
+  createdAt: DateTime;
+  lastModifiedAt: DateTime;
+  key: string; // unique within the project | Any arbitrary string key that uniquely identifies this channel within the project.
+  roles: Array<ChannelRole>; // The roles of this channel. Each channel must have at least one role.
+  name?: LocalizedString; // A human-readable name of the channel.
+  description?: LocalizedString; // A human-readable description of the channel.
+  address?: Address; // The address where this channel is located (e.g. if the channel is a physical store).
+  reviewRatingStatistics?: ReviewRatingStatistics; // Statistics about the review ratings taken into account for this channel.
+  custom?: CustomFields;
+}
+
+export interface ChannelDraft {
+  key: string;
+  roles?: Array<ChannelRole>; // If not specified, then channel will get InventorySupply role by default
+  name?: LocalizedString;
+  description?: LocalizedString;
+  address?: Address;
+  custom?: CustomFieldsDraft;
+}
+
+type ChannelRole = 'InventorySupply' | 'ProductDistribution' | 'OrderExport' | 'OrderImport' | 'Primary';
+
+interface Address {
+  country: string; // A two-digit country code as per ↗ ISO 3166-1 alpha-2 .
+  id?: string;
+  key?: string; // if given it must match [a-zA-Z0-9_\-]{2,256}
+  title?: string;
+  salutation?: string;
+  firstName?: string;
+  lastName?: string;
+  streetName?: string;
+  streetNumber?: string;
+  additionalStreetInfo?: string;
+  postalCode?: string;
+  city?: string;
+  region?: string;
+  state?: string;
+  company?: string;
+  department?: string;
+  building?: string;
+  apartment?: string;
+  pOBox?: string;
+  phone?: string;
+  mobile?: string;
+  email?: string;
+  fax?: string;
+  additionalAddressInfo?: string;
+  externalId?: string;
+}
+
+interface ReviewRatingStatistics {
+  averageRating: number; // Average rating of one target. This number is rounded with 5 decimals.
+  highestRating: number; // Highest rating of one target
+  lowestRating: number; // Lowest rating of one target
+  count: number; // Number of ratings taken into account
+  ratingsDistribution: any; // JSON object. The full distribution of the ratings. The keys are the different ratings and the values are the count of reviews having this rating. Only the used ratings appear in this object.
+}
